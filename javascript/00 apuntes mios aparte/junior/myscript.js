a507c9febf8853776ee063893f83ee437480c56d
juanjose130303 <juanjose140404@gmail.com>
2023-12-01 13:21:05 -0500

    codigo por mi parte 1 dic

---
+++ javascript/00 apuntes mios aparte/junior/myscript.js
@@ -0,0 +1,67 @@
+/*String = "cadena de texto"
+Number = 19
+Boolean = true , false*/
+
+//costante = no pueden cambiar una vez este definida
+
+/*const nombre = "dalto"
+
+nombre = "suarez"
+
+alert(nombre)*/
+
+//declarar
+
+/*let numero;
+
+numero = 29*/ //se tiene que definir para inicializar, let y var se pueden modificar
+
+/*let nombre = prompt("decime tu nombre: "); 
+alert("hola" + nombre)
+*/
+
+//operadores
+
+//asignacion asigna un valor operando de la izqier a la derecha
+
+let numero = 10;
+
+numero += 5;
+
+document.write(numero)
+
+//aritmeticos toman valores numericos  (ya sean literales o variables) como sus operandos y retornan un valor numerico unico.
+
+numero1 = 10;
+
+numero2 = 5;
+
+numero1++;
+
+resultado = numero1;
+
+alert(resultado)
+
+//concatenacion
+saludo = "hola pedro";
+pregunta = "como estas?"
+
+frase = saludo + pregunta;
+
+document.write(frase)
+
+numero1 = 44;
+numero2 = 23;
+
+frase = numero1 + numero2  /*concat es para los strings*/
+
+document.write(frase)
+
+nombre = "juan jose";
+
+frase = `soy  n${nombre} y estoy caminando`;
+
+document.write(frase);
+
+//operadores logicos 
+
